22aae2e5b4f667a885f3c1eed00456a9e3eb729d
gavixcloud <gabevieira@icloud.com>
2021-06-01 15:50:04 -0300

    Site com API Configurada

---
+++ API-projeto-site/models/usuario.js
@@ -0,0 +1,45 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('Usuario',{
+		id: {
+			field: 'idUsuario',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		login: {
+			field: 'usuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		fkNumerologia: {
+			field: 'fkNumerologia',
+			type: DataTypes.INTEGER,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'Usuario', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
